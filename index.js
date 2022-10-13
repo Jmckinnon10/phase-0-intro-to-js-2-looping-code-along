0ddc6b6922e5e77556d2168751cbf2ecc2c6923c
John McKinnon <johnmckinnon23@yahoo.com>
2022-10-13 15:12:39 -0700

    commit tree 2dc29dcffbacabda2601904e3dcc51c3e4602efa

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const cards = ["Guadalupe","Ollie","Aki"];
+function writeCards(cards){
+for(let i = 0; i < cards.length; i++){
+  console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+}
+return cards
+}
+writeCards(cards);
+
+for (let index = 0; index < 10; index--) {
+  console.log(i)
+}
+
+
+
+
+
+
+
+
+
